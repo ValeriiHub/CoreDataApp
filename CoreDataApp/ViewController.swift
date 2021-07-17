9d0fddbe3bc46117934db86d177daf59b157c71a
ValeriiHub <p04taera@gmail.com>
2021-07-17 20:11:06 +0300

    Initial Commit

---
+++ CoreDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by Valerii D on 17.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
